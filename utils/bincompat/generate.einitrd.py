2cc101b43df8a790b6108f74ee24522b1a5db87a
Razvan Deaconescu <razvand@unikraft.io>
2024-01-15 23:26:36 +0200

    fix(utils): Update generate script to new netdev options
    Use new netdev command-line option for generate scripts, e.g.
    `netdev.ip=172.44.0.2/24:172.44.0.1`
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- utils/bincompat/generate.einitrd.py
+++ utils/bincompat/generate.einitrd.py
@@ -294,8 +294,7 @@ def generate_run_fc_json(config, plat, arch, filesystem):
         stream.write(f'    "kernel_image_path": "{kernel}",\n')
         stream.write(f'    "boot_args": "{kernel} ')
         if config["networking"]:
-            stream.write("netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 ")
-            stream.write("netdev.ipv4_subnet_mask=255.255.255.0 ")
+            stream.write("netdev.ip=172.44.0.2/24:172.44.0.1 ")
         if config['rootfs']:
             stream.write('vfs.fstab=[ \\"initrd:/:initrd::extract:\\" ] ')
         if config['cmd']:
@@ -406,9 +405,7 @@ def generate_run_qemu(config, plat, arch, filesystem):
         if config["networking"]:
             stream.write("    -netdev bridge,id=en0,br=virbr0 ")
             stream.write("-device virtio-net-pci,netdev=en0 \\\n")
-            stream.write('    -append "netdev.ipv4_addr=172.44.0.2 ')
-            stream.write("netdev.ipv4_gw_addr=172.44.0.1 ")
-            stream.write("netdev.ipv4_subnet_mask=255.255.255.0 ")
+            stream.write('    -append "netdev.ip=172.44.0.2/24:172.44.0.1 ')
             if config["rootfs"]:
                 if filesystem == "initrd":
                     stream.write('vfs.fstab=[ \\"initrd:/:initrd::extract:\\" ] ')
