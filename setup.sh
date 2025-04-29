6fdacc913e64279b43ee1deae484d082d4b4d174
Razvan Deaconescu <razvand@unikraft.io>
2025-04-29 20:56:37 +0000

    testing: Update library list in setup script
    Add other required libraries in `setup.sh`.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@ test -d repos/libs/lwip || git clone https://github.com/unikraft/lib-lwip repos/
 test -d repos/libs/libcxx || git clone https://github.com/unikraft/lib-libcxx repos/libs/libcxx
 test -d repos/libs/libcxxabi || git clone https://github.com/unikraft/lib-libcxxabi repos/libs/libcxxabi
 test -d repos/libs/libunwind || git clone https://github.com/unikraft/lib-libunwind repos/libs/libunwind
+test -d repos/libs/libuuid || git clone https://github.com/unikraft/lib-libuuid repos/libs/libuuid
+test -d repos/libs/zlib || git clone https://github.com/unikraft/lib-zlib repos/libs/zlib
 test -d repos/libs/compiler-rt || git clone https://github.com/unikraft/lib-compiler-rt repos/libs/compiler-rt
 test -d repos/libs/nginx || git clone https://github.com/unikraft/lib-nginx repos/libs/nginx
+test -d repos/libs/sqlite || git clone https://github.com/unikraft/lib-sqlite repos/libs/sqlite
 test -d repos/libs/python3 || git clone https://github.com/unikraft/lib-python3 repos/libs/python3
+test -d repos/libs/redis || git clone https://github.com/unikraft/lib-redis repos/libs/redis
 test -d repos/libs/libelf || git clone https://github.com/unikraft/lib-libelf repos/libs/libelf
 test -d repos/apps/elfloader || git clone https://github.com/unikraft/app-elfloader repos/apps/elfloader
